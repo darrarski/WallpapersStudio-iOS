fcd613fd3002d2c44d50a6e5b56a80f97f7d9479
Dariusz Rybicki <darrarski@gmail.com>
2020-09-11 15:52:57 +0200

    Replace ScrollView with ZStack and DragGesture

--- WallpapersApp/Sources/ContentView.swift
+++ WallpapersApp/Sources/ContentView.swift
@@ -2,15 +2,19 @@ import SwiftUI
 
 struct ContentView: View {
   @State var isPresentingImagePicker = false
+  @State var hideToolbars: Bool = false
+
   @State var image: UIImage?
+
   @State var imageScale: CGFloat = 1.0
   @State var lastScaleValue: CGFloat = 1.0
-  @State var hideToolbars: Bool = false
+
+  @State var imageOffset: CGPoint = .zero
+  @State var lastOffsetValue: CGPoint = .zero
 
   var body: some View {
     NavigationView {
-      ScrollView([.horizontal, .vertical], showsIndicators: false) {
-        ScrollViewReader { proxy in
+      ZStack {
           if let image = image {
             Image(uiImage: image)
               .scaleEffect(imageScale)
@@ -18,20 +22,31 @@ struct ContentView: View {
                 width: image.size.width * image.scale * imageScale,
                 height: image.size.height * image.scale * imageScale
               )
-              .id(0)
-              .onAppear {
-                proxy.scrollTo(0, anchor: .center)
-              }
+              .offset(x: imageOffset.x, y: imageOffset.y)
           } else {
             Text("No image loaded")
           }
-        }
-      }
-      .edgesIgnoringSafeArea(.all)
-      .onTapGesture {
-        self.hideToolbars.toggle()
       }
       .gesture(
+        DragGesture()
+          .onChanged { value in
+            let offset = CGPoint(
+              x: value.location.x - value.startLocation.x,
+              y: value.location.y - value.startLocation.y
+            )
+            let delta = CGPoint(
+              x: offset.x - self.lastOffsetValue.x,
+              y: offset.y - self.lastOffsetValue.y
+            )
+            self.lastOffsetValue = offset
+            self.imageOffset.x += delta.x
+            self.imageOffset.y += delta.y
+          }
+          .onEnded { value in
+            self.lastOffsetValue = .zero
+          }
+      )
+      .simultaneousGesture(
         MagnificationGesture()
           .onChanged { value in
             let delta = value / self.lastScaleValue
@@ -40,8 +55,12 @@ struct ContentView: View {
           }
           .onEnded { _ in
             self.lastScaleValue = 1.0
-          }
+          },
+        including: .all
       )
+      .onTapGesture {
+        self.hideToolbars.toggle()
+      }
       .navigationTitle("Wallpaper")
       .navigationBarTitleDisplayMode(.inline)
       .navigationBarHidden(hideToolbars)
@@ -49,7 +68,7 @@ struct ContentView: View {
         ToolbarItem(placement: .bottomBar) {
           if !hideToolbars {
             Button(action: { self.isPresentingImagePicker = true }) {
-              Image(systemName: "photo.on.rectangle")
+              Image(systemName: "square.and.arrow.down")
             }
           }
         }
