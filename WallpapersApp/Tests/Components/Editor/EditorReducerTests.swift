2288111ae3d9c88f73d19904b85914eb1d4f7b19
Dariusz Rybicki <darrarski@gmail.com>
2020-09-15 00:05:28 +0200

    Add editor reducer test for loading image

--- WallpapersApp/Tests/Components/Editor/EditorReducerTests.swift
+++ WallpapersApp/Tests/Components/Editor/EditorReducerTests.swift
@@ -38,4 +38,45 @@ final class EditorReducerTests: XCTestCase {
       }
     )
   }
+
+  func testLoadImage() {
+    let image1 = image(color: .red, size: CGSize(width: 6, height: 4))
+    let image2 = image(color: .blue, size: CGSize(width: 3, height: 6))
+
+    store.assert(
+      .send(.loadImage(image1)) {
+        $0.canvas = CanvasState(
+          size: .zero,
+          image: image1,
+          frame: CGRect(origin: .zero, size: image1.size)
+        )
+        $0.menu.isImageLoaded = true
+      },
+      .receive(.canvas(.scaleToFill)) {
+        $0.canvas?.frame.size = .zero
+      },
+      .send(.canvas(.updateSize(CGSize(width: 3, height: 4)))) {
+        $0.canvas?.size = CGSize(width: 3, height: 4)
+      },
+      .receive(.canvas(.scaleToFill)) {
+        $0.canvas?.frame.origin = CGPoint(x: -1.5, y: 0)
+        $0.canvas?.frame.size = image1.size
+      },
+      .send(.loadImage(image2)) {
+        $0.canvas?.image = image2
+        $0.canvas?.frame.origin = .zero
+        $0.canvas?.frame.size = image2.size
+      },
+      .receive(.canvas(.scaleToFill)) {
+        $0.canvas?.frame.origin = CGPoint(x: 0, y: -1)
+      }
+    )
+  }
+
+  private func image(color: UIColor, size: CGSize) -> UIImage {
+    UIGraphicsImageRenderer(size: size).image { context in
+      color.setFill()
+      context.fill(CGRect(origin: .zero, size: size))
+    }
+  }
 }
