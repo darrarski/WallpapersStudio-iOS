390767d3c3b3d2dcc5213b16bf101b0f72ac2970
Dariusz Rybicki <darrarski@gmail.com>
2020-09-14 02:08:23 +0200

    Add initializers to RoundedRect shape

--- WallpapersApp/Sources/Helpers/Shapes/RoundedRect.swift
+++ WallpapersApp/Sources/Helpers/Shapes/RoundedRect.swift
@@ -4,6 +4,16 @@ struct RoundedRect: Shape {
   var corners: UIRectCorner
   var radius: CGSize
 
+  init(corners: UIRectCorner, radius: CGSize) {
+    self.corners = corners
+    self.radius = radius
+  }
+
+  init(corners: UIRectCorner, radius: CGFloat) {
+    self.corners = corners
+    self.radius = CGSize(width: radius, height: radius)
+  }
+
   func path(in rect: CGRect) -> Path {
     Path(UIBezierPath(
       roundedRect: rect,
