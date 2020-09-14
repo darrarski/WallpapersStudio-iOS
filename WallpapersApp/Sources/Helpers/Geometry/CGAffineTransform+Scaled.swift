1cfddfab20ed8625b6c2cfcf3ba2b202bc62e10b
Dariusz Rybicki <darrarski@gmail.com>
2020-09-14 17:53:27 +0200

    Remove unused code

--- WallpapersApp/Sources/Helpers/Geometry/CGAffineTransform+Scaled.swift
+++ WallpapersApp/Sources/Helpers/Geometry/CGAffineTransform+Scaled.swift
@@ -1,21 +1,13 @@
 import CoreGraphics
 
 extension CGAffineTransform {
-  static func scaledBy(x: CGFloat, y: CGFloat, anchor: CGPoint = .zero) -> CGAffineTransform {
-    CGAffineTransform.identity.scaledBy(x: x, y: y, anchor: anchor)
-  }
-
-  func scaledBy(x: CGFloat, y: CGFloat, anchor: CGPoint = .zero) -> CGAffineTransform {
-    translatedBy(x: anchor.x, y: anchor.y)
-      .scaledBy(x: x, y: y)
-      .translatedBy(x: -anchor.x, y: -anchor.y)
-  }
-
   static func scaledBy(_ scale: CGFloat, anchor: CGPoint = .zero) -> CGAffineTransform {
     CGAffineTransform.identity.scaledBy(scale, anchor: anchor)
   }
 
   func scaledBy(_ scale: CGFloat, anchor: CGPoint = .zero) -> CGAffineTransform {
-    scaledBy(x: scale, y: scale, anchor: anchor)
+    translatedBy(x: anchor.x, y: anchor.y)
+      .scaledBy(x: scale, y: scale)
+      .translatedBy(x: -anchor.x, y: -anchor.y)
   }
 }
